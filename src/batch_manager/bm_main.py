3f834699c12508ea5a6f18f34d2cda9e49ca0597
AlessandroSpallina <alessandrospallina1@gmail.com>
2020-09-24 16:58:00 +0200

    batch manager ok

--- src/batch_manager/bm_main.py
+++ src/batch_manager/bm_main.py
@@ -1,22 +1,28 @@
 import os
 import subprocess
+import time
 
 
 def config_paths(path="./configs"):
     to_return = []
 
     for file in os.listdir(path):
-        file_path = os.path.join(path, file)
+        file_path = os.path.abspath(os.path.join(path, file))
         if os.path.isfile(file_path) and '.yaml' in file_path:
-            to_return.append(file_path)
+            to_return.append(file_path.replace('\\', '/'))
     return to_return
 
 
 def main():
     paths = config_paths()
+    start_time = int(time.time())
 
     while len(paths) > 0:
-        process = subprocess.Popen([''])
+        path = paths.pop()
+        process = subprocess.Popen(['../../venv/Scripts/python.exe', '../slicing_core/sc_main.py',
+                                    '-c', path, '-n', f"b-{start_time}/{path.split('/')[-1].split('.')[0]}"])
+        process.communicate()  # this will wait for the process termination
+
 
 if __name__ == '__main__':
     main()
