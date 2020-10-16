0f15adda39e301e714f336b822789f3d01ac6ab1
AlessandroSpallina <alessandrospallina1@gmail.com>
2020-10-16 09:23:27 +0200

    moving to multislice pt1.1

---
+++ src/slicing_core/multi_main.py
@@ -0,0 +1,109 @@
+"""
+"""
+import shutil
+import time
+import logging
+
+import utils
+import plotter
+from agent import Agent
+from slice_mdp import UnitaryAllocationSliceMDP, MultipleAllocationSliceMDP
+from multi_slice_mdp import MultiSliceMDP
+from slice_simulator import SliceSimulator
+import os
+import getopt
+import sys
+
+
+def cli_handler(argv):
+    USAGE = "multislice_main.py -c <configPath> -n <simulationName>"
+    to_return = {}
+    try:
+        # help, config (path), name (directory name of the results)
+        opts, args = getopt.getopt(argv, "hc:n:", ["config=", "name="])
+    except getopt.GetoptError:
+        print(USAGE)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(USAGE)
+            sys.exit()
+        elif opt in ('-c', '--config'):
+            to_return['config'] = arg
+        elif opt in ('-n', '--name'):
+            to_return['name'] = arg
+
+    return to_return
+
+
+def main(argv):
+    cli_args = cli_handler(argv)
+    STORAGE_PATH = f"../../res/exported/{cli_args['name'] if 'name' in cli_args else int(time.time())}/"
+    CONFIG_PATH = cli_args['config'] if 'config' in cli_args else "config.yaml"
+
+    if not os.path.exists(STORAGE_PATH):
+        os.makedirs(STORAGE_PATH)
+    logging.basicConfig(filename=f"{STORAGE_PATH}report.log", level=logging.INFO)
+
+    logging.info(f"Latest commit available at {utils.get_last_commit_link()}")
+
+    shutil.copyfile(CONFIG_PATH, f"{STORAGE_PATH}config.yaml")
+    os.chdir(STORAGE_PATH)
+
+    conf = utils.read_config(path=CONFIG_PATH, verbose=True)
+
+    ARRIVALS = conf['arrivals_histogram']
+    DEPARTURES = conf['departures_histogram']
+    QUEUE_SIZE = conf['queue_size']
+    SERVER_MAX_CAP = conf['server_max_cap']
+
+    # normalize alpha, beta and gamma
+    ALPHA = conf['alpha'] / (conf['alpha'] + conf['beta'] + conf['gamma'])
+    BETA = conf['beta'] / (conf['alpha'] + conf['beta'] + conf['gamma'])
+    GAMMA = conf['gamma'] / (conf['alpha'] + conf['beta'] + conf['gamma'])
+    logging.info(f"Normalized alpha {ALPHA}, beta {BETA} and gamma {GAMMA}")
+
+    C_SERVER = conf['c_server']
+    C_JOB = conf['c_job']
+    C_LOST = conf['c_lost']
+
+    SIMULATIONS = conf['simulations']
+    SIMULATION_TIME = conf['simulation_time']
+    MDP_DISCOUNT_INCREMENT = conf['mdp_discount_increment']
+    DISCOUNT_START_VALUE = conf['mdp_discount_start_value']
+    DISCOUNT_END_VALUE = conf['mdp_discount_end_value']
+    MDP_ALGORITHM = conf['mdp_algorithm']
+
+    DELAYED_ACTION = conf['delayed_action']
+
+    AVERAGE_WINDOW_IN_PLOT = conf['average_window_in_plot']
+
+    ARRIVAL_PROCESSING_PHASES = conf['arrival_processing_phase']
+
+    stats = {}
+
+    time_start = time.time()
+
+    # -------------------
+    slices = [
+        UnitaryAllocationSliceMDP(ARRIVALS, DEPARTURES, QUEUE_SIZE, SERVER_MAX_CAP, alpha=ALPHA, beta=BETA,
+                                  gamma=GAMMA, c_server=C_SERVER, c_job=C_JOB, c_lost=C_LOST,
+                                  algorithm=MDP_ALGORITHM, periods=SIMULATION_TIME,
+                                  delayed_action=DELAYED_ACTION, label="ua",
+                                  arrival_processing_phase=ARRIVAL_PROCESSING_PHASES, verbose=False),
+        MultipleAllocationSliceMDP(ARRIVALS, DEPARTURES, QUEUE_SIZE, SERVER_MAX_CAP, alpha=ALPHA, beta=BETA,
+                                   gamma=GAMMA, c_server=C_SERVER, c_job=C_JOB, c_lost=C_LOST,
+                                   algorithm=MDP_ALGORITHM, periods=SIMULATION_TIME,
+                                   delayed_action=DELAYED_ACTION, label="ma",
+                                   arrival_processing_phase=ARRIVAL_PROCESSING_PHASES, verbose=False)
+    ]
+    multi = MultiSliceMDP(slices)
+    debug = multi._generate_states()
+    for d in debug:
+        print(d[0],d[1])
+
+    # -----------------------
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
--- src/slicing_core/multi_slice_mdp.py
+++ src/slicing_core/multi_slice_mdp.py
@@ -9,9 +9,112 @@ class MultiSliceMDP:
     def __init__(self, slices):
         self._slices = slices
 
+        self._states = self._generate_states()
+        self._transition_matrix = self._generate_transition_matrix()
+        self._reward_matrix = self._generate_reward_matrix()
+
     def _generate_states(self):
         # see https://www.kite.com/python/answers/how-to-get-all-element-combinations-of-two-numpy-arrays-in-python
-        mesh = np.array(np.meshgrid(self._slices[0], self._slices[1]))
-        return mesh.T.reshape(-1, 2)
+        mesh = np.array(np.meshgrid(self._slices[0].states, self._slices[1].states))
+        to_ret = mesh.T.reshape(-1, 2)
+        return to_ret
+
+    @property
+    def transition_matrix(self):
+        return self._transition_matrix
+
+    @property
+    def reward_matrix(self):
+        return self._reward_matrix
+
+    @property
+    def states(self):
+        return self._states
+
+    # ------------------------------------------
+    def _filter_transition_probability_by_action(self, transition_probability, from_state, to_state, action_id):
+        pass
+
+    def _calculate_transition_probability(self, from_state, to_state, action_id):
+        if self._delayed_action:
+            h_d = self._calculate_h_d(from_state)
+        else:
+            h_d = self._calculate_h_d(to_state)
+
+        diff = to_state - from_state
+
+        tmp = 0
+        tmp2 = 0
+
+        for x in range(max(0, diff.k), self._queue_size - from_state.k + 1):
+            p_proc = 0
+
+            try:
+                p_arr = self._arrivals_histogram[x]
+            except IndexError:
+                p_arr = 0
+
+            if from_state.k + x - to_state.k == ((from_state.k + x) if self._arrival_processing_phase else from_state.k):
+                p_proc = sum(h_d[from_state.k + x - to_state.k:])
+            elif from_state.k + x - to_state.k < ((from_state.k + x) if self._arrival_processing_phase else from_state.k):
+                try:
+                    p_proc = h_d[from_state.k + x - to_state.k]
+                except IndexError:
+                    pass
+            else:
+                p_proc = 0
+
+            tmp += p_arr * p_proc
+
+        for x in range(self._queue_size - from_state.k + 1, len(self._arrivals_histogram) + 1):
+            p_proc = 0
+
+            try:
+                p_arr = self._arrivals_histogram[x]
+            except IndexError:
+                p_arr = 0
+
+            if self._queue_size - to_state.k == self._queue_size:
+                p_proc = sum(h_d[self._queue_size - to_state.k:])
+            elif self._queue_size - to_state.k < self._queue_size:
+                try:
+                    p_proc = h_d[self._queue_size - to_state.k]
+                except IndexError:
+                    pass
+            else:
+                p_proc = 0
+
+            tmp2 += p_arr * p_proc
+
+        transition_probability = tmp + tmp2
+
+        return self._filter_transition_probability_by_action(transition_probability, from_state, to_state, action_id)
+
+    """
+    The transition matrix is of dim action_num * states_num * states_num
+    Q[0] -> transition matrix related action 0 (do nothing)
+    Q[1] -> transition matrix related action 0 (allocate 1)
+    Q[2] -> transition matrix related action 0 (deallocate 1)
+    """
+    def _generate_transition_matrix(self):
+        transition_matrix = np.zeros((3, len(self._states), len(self._states)))
+
+        # lets iterate the trans matrix and fill with correct probabilities
+        for a in range(len(transition_matrix)):
+            for i in range(len(self._states)):
+                for j in range(len(self._states)):
+                    transition_matrix[a][i][j] = \
+                        self._calculate_transition_probability(self._states[i], self._states[j], a)
+        return transition_matrix
+    # ------------------------------------------
+
+
+    def _generate_reward_matrix(self):
+        pass
+
+
+
+
+
 
 
