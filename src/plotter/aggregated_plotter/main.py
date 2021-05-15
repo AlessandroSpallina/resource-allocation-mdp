dce11d3343ebc91cfb09d79d1dbc2701832f788d
AlessandroSpallina <alessandrospallina1@gmail.com>
2021-05-15 18:16:29 +0200

    added cost detaild in aggregated plot

--- src/plotter/aggregated_plotter/main.py
+++ src/plotter/aggregated_plotter/main.py
@@ -8,14 +8,14 @@ import src.plotter.utils as utils
 from src.plotter.main import *
 
 
-BATCH_RESULT_PATH = "C:/Users/SK3LA/PycharmProjects/Slicing-5G-MDP/src/slicing_core/exported/results/b-1620296145/"
-OUTPUT_DIRECTORY_PATH = f"../../../res/plots/aggregated-{BATCH_RESULT_PATH.split('/')[-2]}/"
+BATCH_RESULT_PATH = "C:/Users/SK3LA/PycharmProjects/Slicing-5G-MDP/res/RISULTATI/cjob_x5_in_high_prio/result/"
+OUTPUT_DIRECTORY_PATH = f"../../../res/RISULTATI/cjob_x5_in_high_prio/plot/aggregated-{BATCH_RESULT_PATH.split('/')[-2]}/"
 
 
 def get_results_path(start_path=BATCH_RESULT_PATH):
     to_ret = []
 
-    for file in os.listdir(BATCH_RESULT_PATH):
+    for file in os.listdir(start_path):
         if os.path.isdir(os.path.join(start_path, file)):
             to_ret.append(os.path.join(start_path, file, 'results.data'))
 
@@ -57,7 +57,8 @@ def main():
                         'incoming_jobs': sum(slices[0]['incoming_jobs']),
                         'lost_jobs': sum(slices[0]['lost_jobs']),
                         'toolate_jobs': toolate_jobs,
-                        'cost': sum(slices[0]['cost'])
+                        'cost': sum(slices[0]['cost']),
+                        'cost_component': np.array(slices[0]['cost_component']).sum(0).tolist()
                      }
                 )
 
@@ -67,7 +68,8 @@ def main():
                         'incoming_jobs': sum(slices[1]['incoming_jobs']),
                         'lost_jobs': sum(slices[1]['lost_jobs']),
                         'toolate_jobs': sum(slices[1]['processed_jobs']) * sum(slices[1]['wait_time_in_the_system'][6:]),
-                        'cost': sum(slices[1]['cost'])
+                        'cost': sum(slices[1]['cost']),
+                        'cost_component': np.array(slices[1]['cost_component']).sum(0).tolist()
                     }
                 )
 
@@ -81,6 +83,14 @@ def main():
         processed_on_time_percent = [[], []]
         processed_too_late_percent = [[], []]
         lost_percent = [[], []]
+
+        cost_job = [[], []]
+        cost_server = [[], []]
+        cost_lost = [[], []]
+        cost_alloc = [[], []]
+        cost_dealloc = [[], []]
+
+        costs = [[], []]
 
         for s in policy['stats_per_slice']:
             servers_list.append(s['server_cap'])
@@ -97,6 +107,24 @@ def main():
             processed_on_time_percent[0].append((s['slices'][0]['processed_jobs'] * 100) / s['slices'][0]['incoming_jobs'])
             processed_on_time_percent[1].append((s['slices'][1]['processed_jobs'] * 100) / s['slices'][1]['incoming_jobs'])
 
+            cost_job[0].append(s['slices'][0]['cost_component'][0])
+            cost_job[1].append(s['slices'][1]['cost_component'][0])
+
+            cost_server[0].append(s['slices'][0]['cost_component'][1])
+            cost_server[1].append(s['slices'][1]['cost_component'][1])
+
+            cost_lost[0].append(s['slices'][0]['cost_component'][2])
+            cost_lost[1].append(s['slices'][1]['cost_component'][2])
+
+            cost_alloc[0].append(s['slices'][0]['cost_component'][3])
+            cost_alloc[1].append(s['slices'][1]['cost_component'][3])
+
+            cost_dealloc[0].append(s['slices'][0]['cost_component'][4])
+            cost_dealloc[1].append(s['slices'][1]['cost_component'][4])
+
+            costs[0].append(s['slices'][0]['cost'])
+            costs[1].append(s['slices'][1]['cost'])
+
         dataframes.append(
             {
                 'name': policy['name'],
@@ -105,13 +133,25 @@ def main():
                         'server': servers_list,
                         'processed_on_time': processed_on_time_percent[0],
                         'processed_too_late': processed_too_late_percent[0],
-                        'lost': lost_percent[0]
+                        'lost': lost_percent[0],
+                        'cost_job': cost_job[0],
+                        'cost_server': cost_server[0],
+                        'cost_lost': cost_lost[0],
+                        'cost_alloc': cost_alloc[0],
+                        'cost_dealloc': cost_dealloc[0],
+                        'cost_sum': costs[0]
                     }),
                     pd.DataFrame({  # slice low priority
                         'server': servers_list,
                         'processed_on_time': processed_on_time_percent[1],
                         'processed_too_late': processed_too_late_percent[1],
-                        'lost': lost_percent[1]
+                        'lost': lost_percent[1],
+                        'cost_job': cost_job[1],
+                        'cost_server': cost_server[1],
+                        'cost_lost': cost_lost[1],
+                        'cost_alloc': cost_alloc[1],
+                        'cost_dealloc': cost_dealloc[1],
+                        'cost_sum': costs[1]
                     })  # .sort_values(by=["server"]).set_index("server")
                 ]
             }
@@ -127,23 +167,23 @@ def main():
             title = f'Policy {dataframe["name"]} - {"High Priority Slice" if slice_i == 0 else "Low Priority Slice"}\n\n'
 
             # STACKED PLOT SINGLE SLICE
-            ax = dataframe['slices'][slice_i].plot(
-                kind='barh',
-                stacked=True,
-                title=title,
-                figsize=(15, 10),
-                width=0.8
-            )
-            ax.xaxis.grid(color='gray', linestyle='dashed')
-            xticks = np.arange(0, 101, 10)
-            xlabels = ['{}%'.format(i) for i in np.arange(0, 101, 10)]
-            plt.xticks(xticks, xlabels)
-            plt.legend(bbox_to_anchor=(0., 1.01, 1., .102), loc='lower center', ncol=3, borderaxespad=0., frameon=False)
-            plt.savefig(f"{OUTPUT_DIRECTORY_PATH}{dataframe['name']}-slice{slice_i}_stacked")
+            # ax = dataframe['slices'][slice_i][['processed_on_time', 'processed_too_late', 'lost']].plot(
+            #     kind='barh',
+            #     stacked=True,
+            #     title=title,
+            #     figsize=(15, 10),
+            #     width=0.8
+            # )
+            # ax.xaxis.grid(color='gray', linestyle='dashed')
+            # xticks = np.arange(0, 101, 10)
+            # xlabels = ['{}%'.format(i) for i in np.arange(0, 101, 10)]
+            # plt.xticks(xticks, xlabels)
+            # plt.legend(bbox_to_anchor=(0., 1.01, 1., .102), loc='lower center', ncol=3, borderaxespad=0., frameon=False)
+            # plt.savefig(f"{OUTPUT_DIRECTORY_PATH}{dataframe['name']}-slice{slice_i}_stacked")
             # -------------------------
 
             # BARH PLOT NON STACKED SINGLE SLICE
-            ax = dataframe['slices'][slice_i].plot(
+            ax = dataframe['slices'][slice_i][['processed_on_time', 'processed_too_late', 'lost']].plot(
                 kind='bar',
                 stacked=False,
                 title=title,
@@ -170,15 +210,46 @@ def main():
             for i in ax.patches:
                 # get_x pulls left or right; get_height pushes up or down
                 ax.text(i.get_x() - .03, i.get_height() + .5,
-                        str(round((i.get_height()), 2)) + '%', fontsize=9, color='black', rotation=45)
+                        str(round((i.get_height()), 4)) + '%', fontsize=9, color='black', rotation=45)
 
             plt.savefig(f"{OUTPUT_DIRECTORY_PATH}{dataframe['name']}-slice{slice_i}")
+
+
+            # ax = dataframe['slices'][slice_i][['cost_job', 'cost_server', 'cost_lost', 'cost_alloc', 'cost_dealloc']].plot(
+            #     kind='bar',
+            #     stacked=False,
+            #     title=title,
+            #     figsize=(25, 10),
+            #     width=0.8,
+            # )
+            # ax.yaxis.grid(color='gray', linestyle='dashed')
+            # ax.set_ylabel("cost (usd)")
+            #
+            # plt.legend(bbox_to_anchor=(0., 1.01, 1., .102), loc='lower center', ncol=5, borderaxespad=0., frameon=False)
+            #
+            # # create a list to collect the plt.patches data
+            # totals = []
+            #
+            # # find the values and append to list
+            # for i in ax.patches:
+            #     totals.append(i.get_height())
+            #
+            # # set individual bar lables using above list
+            # total = sum(totals)
+            #
+            # # set individual bar lables using above list
+            # for i in ax.patches:
+            #     # get_x pulls left or right; get_height pushes up or down
+            #     ax.text(i.get_x() - .03, i.get_height() + .5,
+            #             str(round((i.get_height()), 4)) + '%', fontsize=9, color='black', rotation=45)
+            #
+            # plt.savefig(f"{OUTPUT_DIRECTORY_PATH}{dataframe['name']}-slice{slice_i}_costs")
             # -------------------------
 
         # TWO SLICES IN ONE FIGURE
 
-        df0 = dataframe['slices'][0]
-        df1 = dataframe['slices'][1]
+        df0 = dataframe['slices'][0][['processed_on_time', 'processed_too_late', 'lost']]
+        df1 = dataframe['slices'][1][['processed_on_time', 'processed_too_late', 'lost']]
 
         fig, axs = plt.subplots(2, 1, constrained_layout=True)
         axs[0].set_title("High Priority", loc="left")
@@ -194,6 +265,7 @@ def main():
             legend=False
         )
         axs[0].legend(bbox_to_anchor=(0., 1.01, 1., .102), loc='lower right', ncol=3, borderaxespad=0., frameon=False)
+        axs[0].grid(axis='y', which='both', color='grey', linestyle='dashed')
         df1.plot(
             kind='bar',
             stacked=False,
@@ -202,75 +274,63 @@ def main():
             legend=False
         )
         axs[1].legend(bbox_to_anchor=(0., 1.01, 1., .102), loc='lower right', ncol=3, borderaxespad=0., frameon=False)
-
+        axs[1].grid(axis='y', which='both', color='grey', linestyle='dashed')
         plt.savefig(f"{OUTPUT_DIRECTORY_PATH}{dataframe['name']}")
 
+        df0 = dataframe['slices'][0][['cost_job', 'cost_server', 'cost_lost', 'cost_alloc', 'cost_dealloc']]
+        df1 = dataframe['slices'][1][['cost_job', 'cost_server', 'cost_lost', 'cost_alloc', 'cost_dealloc']]
+        fig, axs = plt.subplots(2, 1, constrained_layout=True)
+        axs[0].set_title("High Priority", loc="left")
+        axs[1].set_title("Low Priority", loc="left")
+        axs[0].set_ylabel("Costs (USD)")
+        axs[1].set_ylabel("Costs (USD)")
+        fig.suptitle(f"Policy {dataframe['name']}")
+        df0.plot(
+            kind='bar',
+            stacked=False,
+            width=0.8,
+            ax=axs[0],
+            legend=False
+        )
+        axs[0].legend(bbox_to_anchor=(1.05, 1), ncol=3, loc='lower right', borderaxespad=0., frameon=False)
+        axs[0].grid(axis='y', which='both', color='grey', linestyle='dashed')
+        df1.plot(
+            kind='bar',
+            stacked=False,
+            width=0.8,
+            ax=axs[1],
+            legend=False
+        )
+        axs[1].legend(bbox_to_anchor=(1.05, 1), ncol=3, loc='lower right', borderaxespad=0., frameon=False)
+        axs[1].grid(axis='y', which='both', color='grey', linestyle='dashed')
+        plt.savefig(f"{OUTPUT_DIRECTORY_PATH}{dataframe['name']}_cost_component")
 
-        # for r in range(nrow):
-        #     df_list[count].plot(
-        #         kind='bar',
-        #         stacked=False,
-        #         #figsize=(25, 10),
-        #         width=0.8,
-        #         ax=axes[r]
-        #     )
-        #     count += 1
+        df0 = dataframe['slices'][0][['cost_sum']]
+        df1 = dataframe['slices'][1][['cost_sum']]
+        fig, axs = plt.subplots(2, 1, constrained_layout=True)
+        axs[0].set_title("High Priority", loc="left")
+        axs[1].set_title("Low Priority", loc="left")
+        axs[0].set_ylabel("Costs (USD)")
+        axs[1].set_ylabel("Costs (USD)")
+        fig.suptitle(f"Policy {dataframe['name']}")
+        df0.plot(
+            kind='bar',
+            stacked=False,
+            ax=axs[0],
+            legend=False
+        )
+        axs[0].legend(bbox_to_anchor=(0., 1.01, 1., .102), loc='lower right', borderaxespad=0., frameon=False)
+        axs[0].grid(axis='y', which='both', color='grey', linestyle='dashed')
+        df1.plot(
+            kind='bar',
+            stacked=False,
+            ax=axs[1],
+            legend=False
+        )
+        axs[1].legend(bbox_to_anchor=(0., 1.01, 1., .102), loc='lower right', borderaxespad=0., frameon=False)
+        axs[1].grid(axis='y', which='both', color='grey', linestyle='dashed')
+        plt.savefig(f"{OUTPUT_DIRECTORY_PATH}{dataframe['name']}_costs")
 
-
-
-        # ------------------------
-
-    # cost_dataframes = []
-
-    # for policy in policies_aggregated_stats:
-    #     servers_list = []
-    #     cost = [[], []]
-    #     processed_too_late_percent = [[], []]
-    #     lost_percent = [[], []]
-    #
-    #     for s in policy['stats_per_slice']:
-    #         servers_list.append(s['server_cap'])
-    #
-    #         lost_percent[0].append((s['slices'][0]['lost_jobs'] * 100) / s['slices'][0]['incoming_jobs'])
-    #         lost_percent[1].append((s['slices'][1]['lost_jobs'] * 100) / s['slices'][1]['incoming_jobs'])
-    #
-    #         processed_too_late_percent[0].append(
-    #             (s['slices'][0]['toolate_jobs'] * 100) / s['slices'][0]['incoming_jobs'])
-    #         processed_too_late_percent[1].append(
-    #             (s['slices'][1]['toolate_jobs'] * 100) / s['slices'][1]['incoming_jobs'])
-    #
-    #         s['slices'][0]['processed_jobs'] -= s['slices'][0]['toolate_jobs']
-    #         s['slices'][1]['processed_jobs'] -= s['slices'][1]['toolate_jobs']
-    #
-    #         processed_on_time_percent[0].append(
-    #             (s['slices'][0]['processed_jobs'] * 100) / s['slices'][0]['incoming_jobs'])
-    #         processed_on_time_percent[1].append(
-    #             (s['slices'][1]['processed_jobs'] * 100) / s['slices'][1]['incoming_jobs'])
-    #
-    #     dataframes.append(
-    #         {
-    #             'name': policy['name'],
-    #             'slices': [
-    #                 pd.DataFrame({  # slice high priority
-    #                     'server': servers_list,
-    #                     'processed_on_time': processed_on_time_percent[0],
-    #                     'processed_too_late': processed_too_late_percent[0],
-    #                     'lost': lost_percent[0]
-    #                 }),
-    #                 pd.DataFrame({  # slice low priority
-    #                     'server': servers_list,
-    #                     'processed_on_time': processed_on_time_percent[1],
-    #                     'processed_too_late': processed_too_late_percent[1],
-    #                     'lost': lost_percent[1]
-    #                 })  # .sort_values(by=["server"]).set_index("server")
-    #             ]
-    #         }
-    #     )
-    #
-    #     dataframes[-1]['slices'][0]['server'] = dataframes[-1]['slices'][0].astype(int)
-    #     dataframes[-1]['slices'][1]['server'] = dataframes[-1]['slices'][1].astype(int)
-    #     dataframes[-1]['slices'][0] = dataframes[-1]['slices'][0].sort_values(by=["server"]).set_index("server")
-    #     dataframes[-1]['slices'][1] = dataframes[-1]['slices'][1].sort_values(by=["server"]).set_index("server")
 
 
 
